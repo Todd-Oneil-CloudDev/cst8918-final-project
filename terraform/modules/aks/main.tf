47435e4c57a7dd013cf1b31888d769be3e5f8641
Todd Oneil <onei0240@algonquinlive.com>
2026-08-09 19:09:54 -0400

    fixed configuration bugs with conflicting ips

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -2,7 +2,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
   resource_group_name = var.resource_group_name
   location            = var.region
-  kubernetes_version = var.aks_version
+  kubernetes_version  = var.aks_version
+  dns_prefix          = var.dns_prefix
 
   default_node_pool {
     name                = var.node_name
@@ -10,7 +11,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
     enable_auto_scaling = true
     min_count           = var.min
     max_count           = var.max
-    node_count          = var.min
+    vnet_subnet_id      = var.subnet_id
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    dns_service_ip = var.service_dns_ip
+    service_cidr   = var.service_cidr
   }
 
   identity {
--- terraform/modules/aks/variables.tf
+++ terraform/modules/aks/variables.tf
@@ -11,8 +11,8 @@ variable "aks_name" {
   type        = string
   description = "name supplied by the calling environment"
   validation {
-    condition     = length(var.name) > 0
-    error_message = "resource_group_name must not be empty"
+    condition     = length(var.aks_name) > 0
+    error_message = "aks_name must not be empty"
   }
 }
 
@@ -20,8 +20,8 @@ variable "node_name" {
   type        = string
   description = "node name supplied by the calling environment"
   validation {
-    condition     = length(var.name) > 0
-    error_message = "resource_group_name must not be empty"
+    condition     = length(var.node_name) > 0
+    error_message = "node_name must not be empty"
   }
 }
 
@@ -30,7 +30,7 @@ variable "region" {
   description = "region supplied by the calling environment"
   validation {
     condition     = length(var.region) > 0
-    error_message = "resource_group_name must not be empty"
+    error_message = "region must not be empty"
   }
 }
 
@@ -56,4 +56,33 @@ variable "aks_version" {
   type        = string
   description = "version supplied by the calling environment, defaults to 1.32"
   default     = "1.32"
+}
+
+variable "subnet_id" {
+  type = string
+  description = "subnet supplied by the calling environment"
+  validation {
+    condition     = length(var.subnet_id) > 0
+    error_message = "subnet_idmust not be empty"
+  }
+}
+
+variable "dns_prefix" {
+  type        = string
+  description = "version supplied by the calling environment, defaults to 1.32"
+  default     = "1.35"
+  validation {
+    condition     = length(var.dns_prefix) > 0
+    error_message = "dns_prefix must not be empty"
+  }
+}
+
+variable "service_cidr" {
+  type        = string
+  description = "cidr for the internal kubernetes network supplied by the calling environment"
+}
+
+variable "service_dns_ip" {
+  type        = string
+  description = "dns ip for the internal kubernetes network supplied by the calling environment"
 }
