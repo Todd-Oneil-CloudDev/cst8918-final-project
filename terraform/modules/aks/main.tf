4734bcd7020473521dfd79cf0014cafc1ad25a85
Todd Oneil <onei0240@algonquinlive.com>
2026-08-09 15:10:07 -0400

    aks module

---
+++ terraform/modules/aks/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  resource_group_name = var.resource_group_name
+  location            = var.region
+
+  default_node_pool {
+    name                = var.node_name
+    vm_size             = "Standard_B2s"
+    enable_auto_scaling = true
+    min_count           = var.min
+    max_count           = var.max
+    node_count          = var.min
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ terraform/modules/aks/variables.tf
@@ -0,0 +1,53 @@
+variable "resource_group_name" {
+  type        = string
+  description = "resource group supplied by the calling environment"
+  validation {
+    condition     = length(var.resource_group_name) > 0
+    error_message = "resource_group_name must not be empty"
+  }
+}
+
+variable "aks_name" {
+  type        = string
+  description = "name supplied by the calling environment"
+  validation {
+    condition     = length(var.name) > 0
+    error_message = "resource_group_name must not be empty"
+  }
+}
+
+variable "node_name" {
+  type        = string
+  description = "node name supplied by the calling environment"
+  validation {
+    condition     = length(var.name) > 0
+    error_message = "resource_group_name must not be empty"
+  }
+}
+
+variable "region" {
+  type        = string
+  description = "region supplied by the calling environment"
+  validation {
+    condition     = length(var.region) > 0
+    error_message = "resource_group_name must not be empty"
+  }
+}
+
+variable "sku" {
+  type        = string
+  description = "sku supplied by the calling environment, defaults to Standard"
+  default     = "Standard"
+}
+
+variable "min" {
+  type        = number
+  description = "min number of nodes supplied by the calling environment, defaults to 1"
+  default     = 1
+}
+
+variable "max" {
+  type        = number
+  description = "max number of nodes supplied by the calling environment, defaults to 1"
+  default     = 1
+}
